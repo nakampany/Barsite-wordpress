50b84dc9b1f3a4f12c71c0c860029abb3d1ac70b
naka <nakasyo2792@gmail.com>
2022-12-15 07:05:36 +0900

    blog修正

--- archive.php
+++ archive.php
@@ -5,34 +5,32 @@
 
     <div class="text">
         <h1 class="main-text">BLOG</h1>
-        <p class="sub-text">ブログカテゴリー</p>
+        <p class="sub-text">ブログ一覧</p>
     </div>
     <hr>
 
 
-
-    <div class="row g-4 fadeUpTrigger">
+    <!-- ブログ一覧ページ -->
+    <h1 class="heading fadeUpTrigger">BLOG</h1>
+    <h5 class="sub-heading text-center fadeUpTrigger">ブログ</h5>
+    <div class="row g-5 fadeUpTrigger" style="margin:100px 0;">
         <?php if (have_posts()) : ?>
             <?php query_posts('posts_per_page=12'); ?>
             <?php while (have_posts()) : the_post() ?>
-                <article class="col-lg-4 col-sm-6" id="<?php the_ID(); ?>" <?php post_class('news'); ?>>
-                    <a href="<?php the_permalink() ?>">
-                        <div class="project pro-1">
-                            <?php if (has_post_thumbnail()) : ?>
-                                <?php the_post_thumbnail('full'); ?>
-                            <?php else : ?>
-                                <img src="https://barcloud-fukui.com/wp-content/uploads/2022/12/BH7C6659-scaled.jpg" alt="">
-                            <?php endif; ?>
-                            <div class="overlay">
-                                <br>
-                                <h6 class="text-white p-1">
-                                    <?php the_title(); ?>
-                                    <br><time datetime="<?php the_time('Y-m-d') ?>"><?php the_time('Y年m月d日(l)'); ?></time>
-                                </h6>
-                            </div>
+                <a class="col-lg-4 col-sm-6" href="<?php the_permalink() ?>" id="<?php the_ID(); ?>" <?php post_class('news'); ?>>
+                    <div class="project pro-1">
+                        <?php if (has_post_thumbnail()) : ?>
+                            <?php the_post_thumbnail('full'); ?>
+                        <?php else : ?>
+                            <img src="https://barcloud-fukui.com/wp-content/uploads/2022/12/BH7C6659-scaled.jpg" alt="">
+                        <?php endif; ?>
+                        <h5 class="project-name py-4" style="font-weight: 800;"><?php the_title(); ?></h5>
+                        <div>
+                            <p class="project-category" style="font-weight: 500;"><?php the_excerpt(); ?></p>
+                            <time datetime="<?php the_time('Y-m-d') ?>" class="text-end py-1"><?php the_time('Y.m.d'); ?></time>
                         </div>
-                    </a>
-                </article>
+                    </div>
+                </a>
             <?php
             endwhile;
             ?>
